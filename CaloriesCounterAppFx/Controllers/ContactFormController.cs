0ac2fb8d980ed450742d1ab610174898f7eca375
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-05-26 11:35:58 +0100

    backup

---
+++ CaloriesCounterAppFx/Controllers/ContactFormController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CaloriesCounterAppFx.Models;
+
+namespace CaloriesCounterAppFx.Controllers
+{
+    public class ContactFormController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Id,From,Subject,Message")] ContactFormMessage contactFormMessage)
+        {
+            contactFormMessage.To = "admin@localhost";
+            contactFormMessage.SendingDate = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                TempData["Success"] = "Message sent successfully! Congratulations!";
+                db.ContactFormMessages.Add(contactFormMessage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMsg"] = "";
+
+                foreach (var items in ModelState.Values)
+                {
+                    foreach (var er in items.Errors)
+                    {
+
+                        TempData["ErrorMsg"] = TempData["ErrorMsg"].ToString() + " " + er.ErrorMessage.ToString();
+                    }
+
+                }
+
+                return View(contactFormMessage);
+            }
+
+            return View(contactFormMessage);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- CaloriesCounterAppFx/Models/ContactFormMessage.cs
+++ CaloriesCounterAppFx/Models/ContactFormMessage.cs
@@ -10,16 +10,16 @@ namespace CaloriesCounterAppFx.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "We have to know who send the message!")]
         [DataType(DataType.EmailAddress)]
         [EmailAddress]
-        [Display(Name = "Email address")]
+        [Display(Name = "Your email address")]
         public string From { get; set; }
         public string To { get; set; }
-        [Required]
+        [Required(ErrorMessage = "And don't forget to tell us what you are going to talk about...")]
         [DataType(DataType.Text)]
         public string Subject { get; set; }
-        [Required]
+        [Required(ErrorMessage = "So say it... just use plain and short words, our monkeys are just starting their training for customer support.")]
         [DataType(DataType.Text)]
         public string Message { get; set; }
         public DateTime SendingDate { get; set; }
